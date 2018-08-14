5aa7a9599a835e600a23b1dd170a4cd51457502a
gkpeel <gkpeel@gmail.com>
2018-08-13 23:11:12 -0400

    updated game logic

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,3 +1,5 @@
+// ====== VARIABLES ====== //
+
 var secretWords = [
     "jambalaya", 
     "lasagna", 
@@ -10,36 +12,93 @@ var secretWords = [
 ];
 var guessedLetters = [];
 var moves2go = 13;
+var word2guess = randomWord(secretWords);
+var guessOutput = guessDisplay(word2guess);
 
 
+
+// ====== FUNCTIONS ====== //
 function randomWord(dictionary) {
     return dictionary[Math.floor(Math.random() * dictionary.length)];
 }
 
-function initialDisplay(secretWord) {
+function guessDisplay(secretWord) {
     var blankWord = "";
     for (var i=0; i<secretWord.length; i++) {
         if (secretWord[i] === " ") {
-            blankWord += "   ";
+            blankWord += " ";
         } else {
-            blankWord += " _ ";
+            blankWord += "_";
         }
     }
     return blankWord;
 }
 
-function guessDisplay(secretWord) {
-    init = initialDisplay(secretWord);
-    return init;
+function alreadyGuessed(userInput, guessedLetters) {
+    if (guessedLetters.includes(userInput)) {
+        return true;
+    } else {
+        return false;
+    }
 }
 
-function checkIfGuessed(userInput, lettersGuessed) {
-    if (lettersGuessed.includes(userInput)) {
+function updateOutput (userInput, word2guess, guessOutput) {
+    for (var i=0; i<word2guess.length; i++) {
+        if (userInput === word2guess[i]) {
+            guessOutput = reWriteOutput(userInput, guessOutput, i);
+        }
+    }
+    return guessOutput;
+}
+
+function reWriteOutput(userInput, guessOutput, i) {
+    retval = guessOutput.substr(0, i) + userInput + guessOutput.substr(i+1, guessOutput.length);
+    return retval;
+}
+
+function check(userInput, word2guess, guessedLetters, guessOutput) {
+    // checks to see if the typed key is in the word and not already guessed
+    if (word2guess.includes(userInput) && !alreadyGuessed(userInput, guessedLetters)) {
+        console.log("true");
+        guessOutput = updateOutput(userInput, word2guess, guessOutput);
+        guessedLetters.push(userInput);
+    } else if (alreadyGuessed(userInput, guessedLetters)) {
     } else {
         guessedLetters.push(userInput);
         moves2go --;
     }
+    return guessOutput
 }
+
+
+console.log(word2guess);
+
+console.log(guessOutput.length);
+
+document.onkeyup = function(event) {
+
+    userInput = event.key;
+    guessOutput = check(userInput, word2guess, guessedLetters, guessOutput);
+
+    console.log(userInput);
+    console.log(guessOutput);
+    console.log(guessedLetters);
+    console.log(moves2go);
+
+
+}
+
+
+
+
+
+//dictionary - all the possible words to guess
+//secretWord - randomly selected from the dictionary
+
+//unknown 
+
+
+
 
 // function checkWord(userInput, word2guess) {
 //     // check if userInput not in lettersGuessed array
@@ -61,37 +120,24 @@ function checkIfGuessed(userInput, lettersGuessed) {
 //         // add userInput to lettersGuessed array
 //         guessedLetters.push(userInput);
 //     } 
-//     // else 
-//         // add userInput to lettersGuessed array
 // }
 
 
-var word2guess = randomWord(secretWords);
-console.log(word2guess);
-var guessOutput = guessDisplay(word2guess);
+// var wordGuessGame = {
+//     dictionary: [
+//         "jambalaya", 
+//         "lasagna", 
+//         "cucumber", 
+//         "artichoke", 
+//         "pulled pork", 
+//         "osso bucco", 
+//         "ramen", 
+//         "pho dac biet"
+//     ], 
+//     word2guess: function() {
+//         this.dictionary[Math.floor(Math.random() * dictionary.length)]
+//     },
+//     initialDisplay: function(word2guess) {
 
-
-document.onkeyup = function(event) {
-
-    userInput = event.key;
-    checkWord(userInput, word2guess);
-    
-
-    // checkIfGuessed(userInput);
-
-    console.log(userInput);
-    console.log(guessOutput);
-    console.log(guessedLetters);
-    console.log(moves2go);
-
-
-}
-
-
-
-
-
-//dictionary - all the possible words to guess
-//secretWord - randomly selected from the dictionary
-
-//unknown 
+//     }
+// }
